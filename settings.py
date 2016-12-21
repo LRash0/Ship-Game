ca17c3e8719770ad1f56f3103693f995577fc577
LRash0 <lucasdrf92@gmail.com>
2016-12-21 14:18:42 -0300

    Criando uma classe para gerenciar as opções.

---
+++ settings.py
@@ -0,0 +1,23 @@
+class Settings():
+	"""Uma classe para guardar todas as configurações da Invasão Alienígena."""
+
+
+	def __init__(self):
+		"""Inicializa as configurações do jogo."""
+		# Configuração da tela
+
+		self.screen_width  = 1200
+		self.screen_height = 800
+		self.bg_color 	   = (230,230,230)
+
+	def get_screen_width(self):
+
+		return self.screen_width
+
+	def get_screen_height(self):
+
+		return self.screen_height
+
+	def get_bg_color(self):
+
+		return self.bg_color
