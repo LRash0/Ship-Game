7c3e8041038768155cd003960a3a2044d39004cf
LRash0 <lucasdrf92@gmail.com>
2016-12-21 15:27:57 -0300

    Adicionando a classe da aeronave e a imagem da nave.

---
+++ ship.py
@@ -0,0 +1,22 @@
+import pygame
+
+class Ship():
+
+	"""Classe da aeronave."""
+
+	def __init__(self,screen):
+		"""Inicializa a espaçonave e define sua posição inicial."""
+		self.screen = screen
+		# Carrega a imagem da espaçonave e obtém seu rect
+		self.image = pygame.image.load('images/ship.bmp')
+		self.rect = self.image.get_rect()
+
+		self.screen_rect = screen.get_rect()
+
+		# Inicializa cada nova espaçonave na pate inferior central da tela
+		self.rect.centerx = self.screen_rect.centerx
+		self.rect.bottom  = self.screen_rect.bottom
+
+	def blitme(self):
+		"""Desenha a espaçonave em sua posição atual."""
+		self.screen.blit(self.image,self.rect)
