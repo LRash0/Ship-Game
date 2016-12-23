f3e44cf1423093c700c63ec691949858d2f5b1d0
LRash0 <lucasdrf92@gmail.com>
2016-12-23 19:34:59 -0300

    Ajeitando as cofigurações básicas.

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ class Settings():
 		
 		# Configurações dos projéteis
 		
-		self.bullet_width  = 300
+		self.bullet_width  = 3
 		self.bullet_height = 15
 		self.bullet_color = 60,60,60
 		self.bullets_allowed = 6
